a37a28b9459408ce373fc682aaa38da5de448b98
Julia Ver <jverchyonova@gmail.com>
2019-11-03 14:29:52 +0200

    basic beauty: logo & metas

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,13 +3,22 @@ import {Link} from "react-router-dom";
 
 const Header = (props) => {
 
+    const styles = {
+        navbarLogo: {
+            width: '7rem',
+            margin: '1rem'
+        }
+    }
+
     return (
         <>
             <header>
                 <div className="container-fluid">
                     <div className="row">
                         <Link to={`/`} className="col-2">
-                            <div>logo</div>
+                            <div style={styles.navbarLogo}>
+                                <img className="img-fluid" src="/img/twear_logo_grey-on-transparent.png" alt="NOT FOUND"/>
+                            </div>
                         </Link>
                         <div className="col-10 text-center">
                             <p>THIS IS HEADER</p>
