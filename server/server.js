08b99996cd031dd58abdc742b112ba3d137951fd
Julia Ver <jverchyonova@gmail.com>
2019-11-07 15:45:59 +0200

    server & mongodb WORK!!! :)

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require("./config/keys").mongoURI
 
 // Connect to MongoDB
 mongoose
