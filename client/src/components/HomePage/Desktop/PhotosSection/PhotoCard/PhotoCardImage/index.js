29ef6c016d8fc9c8ea1de370a7406762803b443e
Yuriy <rededflower@gmail.com>
2019-11-20 21:25:37 +0200

    and again

---
+++ client/src/components/HomePage/Desktop/PhotosSection/PhotoCard/PhotoCardImage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import useStyles from '../../../../useStyles';
+
+const PhotoCardImage = () => {
+
+	const classes = useStyles();
+
+	return(
+		<img src="https://picsum.photos/800/750" />
+		// как временная мера
+	)
+
+}
+
+export default PhotoCardImage
