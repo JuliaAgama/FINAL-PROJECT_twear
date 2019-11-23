bc23992953fce5e5c9dc131a0e684cb39566b1f1
Julia Ver <jverchyonova@gmail.com>
2019-11-23 15:58:12 +0200

    build folder is added

---
+++ client/build/precache-manifest.7a20dd802987f47dc37053890b0c99cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295a3cd4bb271456b2e54e44add31fd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b7f9eb02d7518e3d57f",
+    "url": "/static/css/2.1566cbe7.chunk.css"
+  },
+  {
+    "revision": "0bc0e191d4ac68d49e2f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0b7f9eb02d7518e3d57f",
+    "url": "/static/js/2.f0430139.chunk.js"
+  },
+  {
+    "revision": "0bc0e191d4ac68d49e2f",
+    "url": "/static/js/main.b57ad91d.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
