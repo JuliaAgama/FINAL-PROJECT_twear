ce8dc6278fda9cfb96fa2e3214d5abb85e58b6fe
Julia Ver <jverchyonova@gmail.com>
2019-11-23 16:01:14 +0200

    build folder is added. server.js changed for mainRoute

--- server.js
+++ server.js
@@ -73,17 +73,18 @@ app.use("/comments", comments);
 app.use("/shipping-methods", shippingMethods);
 app.use("/payment-methods", paymentMethods);
 app.use("/partners", partners);
-app.use("/", mainRoute);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
-
+  
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+} else {
+  app.use("/", mainRoute);
+};
 
 const port = process.env.PORT || 5000;
 
