9600405cb2d6a3772a14e7a6f5da63d24244e396
Oleksandr Shtuka <ashtuka@gmail.com>
2019-11-23 12:28:19 +0200

    chenge mongo uri

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://Alex:mongodb@mongolearningdb-dq4cp.mongodb.net/test?retryWrites=true&w=majority"
-  // mongoURI: process.env.MONGO_URI
+  // mongoURI: "mongodb+srv://Alex:mongodb@mongolearningdb-dq4cp.mongodb.net/test?retryWrites=true&w=majority"
+  mongoURI: process.env.MONGO_URI
 };
