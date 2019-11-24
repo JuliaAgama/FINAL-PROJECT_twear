c4982cbf04e0c68e58f8f62585ff0a20f6fb7b88
Oleksandr Shtuka <ashtuka@gmail.com>
2019-11-24 17:05:20 +0200

    deleted unnecessary

--- client/src/index.js
+++ client/src/index.js
@@ -3,27 +3,16 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Router} from './routes/';
 import './index.css';
-import { create } from 'jss';
-import { StylesProvider, jssPreset } from '@material-ui/core/styles';
-// import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
 
 const store = configureStore();
-const jss = create({
-    ...jssPreset(),
-    // Define a custom insertion point that JSS will look for when injecting the styles into the DOM.
-    insertionPoint: document.getElementById('jss-insertion-point'),
-});
-
-
 
 ReactDOM.render(
-    <StylesProvider jss={jss}>
         <Provider store={store}>
             <BrowserRouter>
                 <Router/>
             </BrowserRouter>
         </Provider>
-    </StylesProvider>, document.getElementById('root'));
+    , document.getElementById('root'));
