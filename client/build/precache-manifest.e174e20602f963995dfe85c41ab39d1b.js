215f18c7be5707e13dd81f7625a57ed3584bb706
Oleksandr Shtuka <ashtuka@gmail.com>
2019-11-24 20:00:31 +0200

    after pull

---
+++ client/build/precache-manifest.e174e20602f963995dfe85c41ab39d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103a6803fbdb89f91c115c7de55d5c5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3519a94e4d486d53cfe7",
+    "url": "/static/css/2.1566cbe7.chunk.css"
+  },
+  {
+    "revision": "25851dae7c4b85552b34",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "3519a94e4d486d53cfe7",
+    "url": "/static/js/2.c53dfcb6.chunk.js"
+  },
+  {
+    "revision": "25851dae7c4b85552b34",
+    "url": "/static/js/main.732d67fc.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
