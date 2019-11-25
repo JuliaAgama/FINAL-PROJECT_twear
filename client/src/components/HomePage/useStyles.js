8fd6c4299bf5f4f2b781ac2dccbdaab2401d1191
Yuriy <rededflower@gmail.com>
2019-11-25 15:01:45 +0200

    styles

--- client/src/components/HomePage/useStyles.js
+++ client/src/components/HomePage/useStyles.js
@@ -3,9 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(theme => (
 	{	
 		mainContainer:{
-			[theme.breakpoints.down('sm')]:{
 				padding: '0px',		
-			},
 		},
 
 		sectionContainer:{
