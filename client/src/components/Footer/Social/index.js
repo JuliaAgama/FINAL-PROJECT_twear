0d726fb2e6b6c217175dc216ca5bfd907e141dc8
Oleksandr Shaporda <molenathome@gmail.com>
2019-11-27 13:38:25 +0200

    absolute fix

--- client/src/components/Footer/Social/index.js
+++ client/src/components/Footer/Social/index.js
@@ -10,34 +10,48 @@ export default function Company() {
     <div className={classes.borderNoTop}>
       <Typography>
         <Hidden smDown>
-          <Grid container className={classes.flexMid}>
-            <Link
-              href="#"
-              color={"textPrimary"}
-              style={{ position: "absolute", left: "40px", top: "218px" }}
-            >
-              Instagram
-            </Link>
-            <Link
-              href="#"
-              color={"textPrimary"}
-              style={{ position: "absolute", left: "140px", top: "218px" }}
-            >
-              Facebook
-            </Link>
+          <Grid container className={classes.flexBetween}>
+            {/* <div style={{ display: "flex", flex: "1", justifyContent: "flex-start" }}> */}
+            <div style={{ display: "flex", flex: "1" }}>
+              <Link
+                href="#"
+                color={"textPrimary"}
+                style={{padding:10}}  
+                // style={{ position: "absolute", left: "40px", top: "218px" }}
+              >
+                Instagram
+              </Link>
+              <Link
+                href="#"
+                color={"textPrimary"}
+                style={{padding:10}}
+                // style={{ flex: "1" }}
+                // style={{ position: "absolute", left: "140px", top: "218px" }}
+              >
+                Facebook
+              </Link>
+            </div>
             <Box fontFamily="Monospace" fontSize="h6.fontSize" m={1}>
               © 2019 TWEAR
             </Box>
+            <div style={{ flex: "1" }}></div>
           </Grid>
-
         </Hidden>
         <Hidden mdUp>
           <Grid item className={classes.flexBetween}>
             <div>
-              <Link href="#" color={"textPrimary"} style={{ padding: "0 10px", fontSize: 14 }}>
+              <Link
+                href="#"
+                color={"textPrimary"}
+                style={{ padding: "0 10px", fontSize: 14 }}
+              >
                 Instagram
               </Link>
-              <Link href="#" color={"textPrimary"} style={{ padding: "0 5 px", fontSize: 14 }}>
+              <Link
+                href="#"
+                color={"textPrimary"}
+                style={{ padding: "0 5 px", fontSize: 14 }}
+              >
                 Facebook
               </Link>
             </div>
@@ -45,7 +59,7 @@ export default function Company() {
               <Box
                 fontFamily="Monospace"
                 fontSize="h7.fontSize"
-                style={{ padding: '5px 20px' }}
+                style={{ padding: "5px 20px" }}
               >
                 © 2019 TWEAR
               </Box>
