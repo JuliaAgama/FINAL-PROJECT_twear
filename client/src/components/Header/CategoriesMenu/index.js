2d58acf0d015563a431fd1e9d89de90a9bb6c382
Oleksandr Shtuka <ashtuka@gmail.com>
2019-11-29 11:36:38 +0200

    test add gategories from data base

--- client/src/components/Header/CategoriesMenu/index.js
+++ client/src/components/Header/CategoriesMenu/index.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Button from "@material-ui/core/Button";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {showMenCategoriesAction, showWomenCategoriesAction} from "../../../store/actions/header";
 import useStyles from "./useStyles";
+import {getAllCategories} from "../../../store/actions/categories";
 
 export default function CategoriesMenu(props) {
     const classes = useStyles();
+    const {categories, loaded}  = useSelector(state => state.categories);
     const dispatch = useDispatch();
-    const clickHandler = () => {
-        if (props.isMen){
-            dispatch(showMenCategoriesAction());
-        } else {
-            dispatch(showWomenCategoriesAction());
-        }
-    };
+    useEffect(dispatch(getAllCategories()));
+    console.log(categories);
+    // const clickHandler = () => {
+    //     if (props.isMen){
+    //         dispatch(showMenCategoriesAction());
+    //     } else {
+    //         dispatch(showWomenCategoriesAction());
+    //     }
+    // };
 
     return (
         <React.Fragment>
             <Button className={props.border ?`${classes.btn} ${classes.btnBorder}`: classes.btn}
-                    onClick={clickHandler}
+                    // onClick={clickHandler}
             >
                 {props.title}
             </Button>
