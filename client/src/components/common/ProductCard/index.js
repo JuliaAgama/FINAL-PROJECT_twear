144968d0d57dc42141f897ada7fbbbe08529394d
Oleksandr Shtuka <ashtuka@gmail.com>
2019-12-02 11:42:47 +0200

    add products from db

--- client/src/components/common/ProductCard/index.js
+++ client/src/components/common/ProductCard/index.js
@@ -4,43 +4,44 @@ import { useSpring, animated as a } from 'react-spring'
 import useStyles from "./useStyles";
 
 
-export default function ProductCard() {
+export default function ProductCard(props) {
     const classes = useStyles();
-    const [flipped, set] = useState(false)
+    const [flipped, set] = useState(false);
+   const {name, price, itemNo, srcImg1, srcImg2 } = props;
     const { transform, opacity } = useSpring({
-        opacity: flipped ? 0 : 1,
-        transform: `perspective(600px) rotateY(${flipped ? 0 : 180}deg)`,
+        opacity: flipped ? 1 : 0,
+        transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
         config: { mass: 5, tension: 350, friction: 80 }
     })
-    const imgSrc1 = "https://cdn.shopify.com/s/files/1/0092/9605/2282/products/Brenda_Coat-Jacket-0145-14006-Windsor_Wine_1512x.jpg?v=1570614063";
-    const imgSrc2 = "https://cdn.shopify.com/s/files/1/0092/9605/2282/products/Brenda_Coat-Jacket-0145-14006-Windsor_Wine-1_2048x.jpg?v=1570614071";
+    // const imgSrc1 = "https://cdn.shopify.com/s/files/1/0092/9605/2282/products/Brenda_Coat-Jacket-0145-14006-Windsor_Wine_1512x.jpg?v=1570614063";
+    // const imgSrc2 = "https://cdn.shopify.com/s/files/1/0092/9605/2282/products/Brenda_Coat-Jacket-0145-14006-Windsor_Wine-1_2048x.jpg?v=1570614071";
 
     return (
         <Container maxWidth={false}
                    onMouseEnter={() => set(true)}
                    onMouseLeave={() => set(false)}
                    className={classes.mainContainer}>
-            {flipped ?
+            {!flipped ?
                 <a.div className={classes.anime} style={{opacity: opacity.interpolate(o => 1 - o), transform}}>
                     <div className={classes.imgContainer}>
                         <img className={classes.img}
-                             src={imgSrc2}
+                             src={srcImg1}
                              alt="NOT FOUND"/>
                     </div>
                     <div className={classes.textContainer}>
-                        <p className={classes.title}>Title fsdfsd sdfdsfds sfd</p>
-                        <p className={classes.value}>4500</p>
+                        <p className={classes.title}>{name}</p>
+                        <p className={classes.value}>{price}</p>
                     </div>
                 </a.div>
                 :
                 <a.div className={classes.anime} style={{opacity, transform: transform.interpolate(t => `${t} rotateY(180deg)`)}}>
                     <div className={classes.imgContainer}>
                         <img className={classes.img}
-                             src={imgSrc1}
+                             src={srcImg2}
                              alt="NOT FOUND"/>
                     </div>
                     <div className={classes.textContainer}>
-                        <p className={classes.title}>Size</p>
+                        <p className={classes.title}>{itemNo}</p>
                         <p className={classes.value}>42 41 40 39 38</p>
                     </div>
                 </a.div>
--- client/src/components/common/ProductGallery/index.js
+++ client/src/components/common/ProductGallery/index.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Container} from "@material-ui/core";
 import ProductCard from "../ProductCard";
 import useStyles from "./useStyles";
+import {useDispatch, useSelector} from "react-redux";
+import {getAllProducts} from "../../../store/actions/products";
 
 
 
 export default function ProductGallery() {
     const classes = useStyles();
+    const dispatch = useDispatch();
+    const products = useSelector(state => state.products.products);
+    useEffect(() => {dispatch(getAllProducts())},[]);
+    const productCards = products.map(product => <ProductCard name={product.name}
+                                                              price={product.price}
+                                                              itemNo={product.itemNo}
+                                                              srcImg1={product.imgs[0]}
+                                                              srcImg2={product.imgs[1]}/>);
+    console.log(products);
 
     return (
         <Container maxWidth={false} className={classes.mainContainer}>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
+            {productCards}
         </Container>
     );
 }
