8706b6c8a3a360804a5294638a4f15790269de19
Julia Ver <jverchyonova@gmail.com>
2019-12-02 13:14:21 +0200

    builded

--- client/build/precache-manifest.1511f30d9f4674790b70b15a2a066f16.js
+++ client/build/precache-manifest.1511f30d9f4674790b70b15a2a066f16.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "04fe3208530d193d305e4a283f2ff132",
+    "revision": "400dac51e7d77af8da8ecb3e0112e34c",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.0340a6d1.chunk.css"
   },
   {
-    "revision": "d39d7db629fdf21ccad4",
+    "revision": "78627847353c05a123e7",
     "url": "/static/css/main.34de6062.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.41268c9e.chunk.js"
   },
   {
-    "revision": "d39d7db629fdf21ccad4",
-    "url": "/static/js/main.c3669f84.chunk.js"
+    "revision": "78627847353c05a123e7",
+    "url": "/static/js/main.41d8993c.chunk.js"
   },
   {
     "revision": "875d570ce1b6fc43afc6",
