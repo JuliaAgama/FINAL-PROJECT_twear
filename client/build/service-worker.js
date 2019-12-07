8ccdfb7aed58098a023b8d8afdf3641443d4ea78
Julia Ver <jverchyonova@gmail.com>
2019-12-07 14:28:10 +0200

    builded

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa09607ed4917a5ca9dad38503b2ee4b.js"
+  "/precache-manifest.baab6b4eb5231c8f2bfd8c654077677a.js"
 );
 
 self.addEventListener('message', (event) => {
