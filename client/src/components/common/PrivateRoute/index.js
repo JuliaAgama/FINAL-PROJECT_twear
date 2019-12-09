db8d0268659aeb2a3f0c0e4b151e97787cacb55c
Oleksandr Shtuka <ashtuka@gmail.com>
2019-12-09 22:38:44 +0200

    try fixed privet route

--- client/src/components/common/PrivateRoute/index.js
+++ client/src/components/common/PrivateRoute/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Route, Redirect,} from 'react-router-dom';
 import {useSelector} from "react-redux";
+import Customer from '../../../services/Customer'
 
 export default function PrivateRoute ({ component: Component, ...rest }){
 
   const {customer}  = useSelector(state => state.customers);
+  // const [ isAllowed, setIsAllowed] = useState(null);
+  //
+  // async function user() {
+  //   await new Customer().getCustomer()
+  //     .then(res => setIsAllowed(res.isAdmin));
+  //   return '';
+  // }
 
-  return  <Route {...rest} render={(props) => (
-        customer.isAdmin === true
-            ? <Component {...props} />
-            : <Redirect to='/accessDenied' />
-    )} />
+  return  (
+          <Route {...rest} render={(props) => (
+              customer.isAdmin === true
+                  ? <Component {...props} />
+                  : <Redirect to='/accessDenied' />
+          )} />
+  )
 }
