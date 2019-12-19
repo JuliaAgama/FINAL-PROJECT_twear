90189bf3902e0269cad5347dbd1704d0e6999f99
Julia Ver <jverchyonova@gmail.com>
2019-12-19 14:58:31 +0200

    Admin. Upload. Category Form. Some imrpovements

--- client/src/components/AdminPage/AdminCategories/EditNew/CategoryForm/useStyles.js
+++ client/src/components/AdminPage/AdminCategories/EditNew/CategoryForm/useStyles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { green, grey, pink } from '@material-ui/core/colors';
 
 export default makeStyles(theme => (
     {
@@ -28,6 +29,33 @@ export default makeStyles(theme => (
             backgroundPosition: 'center',
             backgroundSize: 'contain',
             backgroundRepeat: 'no-repeat',
+        },
+        newImg: {
+            position: 'absolute',
+            bottom: '3px',
+            left: '3px',
+            borderRadius: '5px',
+            paddingLeft: theme.spacing(1),
+            paddingRight: theme.spacing(1),
+            backgroundColor: green[600],
+            color: '#fff',
+        },
+        deleteBtn: {
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            transform: 'translate(50%, -50%)',
+            boxSizing: 'content-box',
+            border: `1px solid ${grey[700]}`,
+            borderRadius: '50%',
+            padding: theme.spacing(1),
+            cursor: 'pointer',
+            backgroundColor: '#fff',
+            color: grey[700],
+            '&:hover': {
+                color: pink[700],
+                border: `1px solid ${pink[700]}`,
+            },
         },
         btn: {
             display: 'block',
