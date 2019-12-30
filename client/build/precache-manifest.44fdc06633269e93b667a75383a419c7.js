ea374126b39cf3c7d6c5007c44ac289fc4c09662
Julia Ver <jverchyonova@gmail.com>
2019-12-30 13:26:15 +0200

    builded

--- client/build/precache-manifest.44fdc06633269e93b667a75383a419c7.js
+++ client/build/precache-manifest.44fdc06633269e93b667a75383a419c7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "41bffab9b06cefcebe8a77e22f113b32",
+    "revision": "c215ef466f7b877ddcdf4123b4faab91",
     "url": "/index.html"
   },
   {
-    "revision": "d68d121742d442b5b410",
+    "revision": "4d9da5681574a0b5e93c",
     "url": "/static/css/2.45d426d9.chunk.css"
   },
   {
-    "revision": "95238776766f8b909c72",
+    "revision": "3a9ac8f8f95b36c55852",
     "url": "/static/css/main.d8bcda9e.chunk.css"
   },
   {
-    "revision": "d68d121742d442b5b410",
-    "url": "/static/js/2.42d41c63.chunk.js"
+    "revision": "4d9da5681574a0b5e93c",
+    "url": "/static/js/2.b95ad7ca.chunk.js"
   },
   {
-    "revision": "95238776766f8b909c72",
-    "url": "/static/js/main.5dfaa871.chunk.js"
+    "revision": "3a9ac8f8f95b36c55852",
+    "url": "/static/js/main.30388e06.chunk.js"
   },
   {
     "revision": "875d570ce1b6fc43afc6",
