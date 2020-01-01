10a3c5a89ca271d3b51d354307e9c8027fe60c65
Oleksandr Shtuka <ashtuka@gmail.com>
2020-01-01 19:05:05 +0200

    start productPage

--- client/src/components/ProductPage/ProductPageItem/index.js
+++ client/src/components/ProductPage/ProductPageItem/index.js
@@ -1,21 +1,51 @@
 import React from "react";
-import { Link, Grid, Hidden } from "@material-ui/core";
-// import { ThemeProvider } from "@material-ui/core/styles";
-// import theme from "../../../theme";
 import useStyles from "./useStyles";
+import {useSelector} from "react-redux";
+import {Container} from "@material-ui/core";
+import { Carousel } from "react-responsive-carousel";
+import clamp from 'lodash-es/clamp'
+import { useSpring, animated } from 'react-spring'
+import { useGesture } from 'react-with-gesture'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-
-function CategoryCard(props) {
-  const { name, image } = props;
+function Product() {
   const classes = useStyles();
+  const product = useSelector(state => state.products.product);
+  let imgs =[];
 
-  return (
-    <>
-      <Hidden smDown>
-      <p>something</p>
-      </Hidden>
-    </>
-  );
+    const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }))
+    const bind = useGesture(({ down, delta, velocity }) => {
+        velocity = clamp(velocity, 1, 8)
+        set({ xy: down ? delta : [0, 0], config: { mass: velocity, tension: 500 * velocity, friction: 50 } })
+    })
+
+  if(product.imgs) {
+      imgs = product.imgs.map(item => {
+          return <img key={item}
+                      src={item}
+                      className={classes.img}
+                      alt="Img not found"
+          />
+      })
+  }
+
+    return (
+        <Container maxWidth={false} className={classes.mainContainer} >
+            <Container maxWidth={false} className={classes.imgContainer} >
+                <animated.div {...bind()} style={{ transform: xy.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`) }}>
+                    <Container maxWidth={false} className={classes.imgScale}>
+                        <Carousel showThumbs={false}
+                                  showArrows={false}
+                                  showStatus={false}
+                                  axis="vertical">
+                            {imgs[0] ? imgs : ''}
+                        </Carousel>
+                    </Container>
+                </animated.div>
+            </Container>
+            <Container maxWidth={false} className={classes.container}>DDDDDD</Container>
+        </Container>
+    );
 }
 
-export default CategoryCard;
+export default Product;
--- client/src/components/ProductPage/ProductPageItem/useStyles.js
+++ client/src/components/ProductPage/ProductPageItem/useStyles.js
@@ -1,6 +1,43 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
+import {grey} from "@material-ui/core/colors";
 
-export default makeStyles(theme => ({
+export default makeStyles(theme => (
+    {
+        mainContainer: {
+            display: 'flex',
+            width: '100%',
+            padding: '0',
+            marginTop: '120px',
+            marginBottom: '60px',
+            border: '1px solid black',
+        },
+        container : {
+            width: '50%',
+            borderLeft: '1px solid black',
+            padding: '0',
+            margin: '0',
+        },
+        imgContainer : {
+            width: '50%',
+            padding: '0',
+            margin: '0',
+            overflow: 'hidden'
+        },
+        imgScale: {
+            width: '100%',
+            padding: '0',
+            margin: '0',
+            transition: '0.5s',
+            "&:hover": {
+                transform: 'scale(1.9)',
+                cursor: 'pointer'
+            },
+        },
+        img: {
+            "&:hover": {
+                transform: 'scale(1.2)',
+            },
+        }
+    }
+));
 
-    
-}));
