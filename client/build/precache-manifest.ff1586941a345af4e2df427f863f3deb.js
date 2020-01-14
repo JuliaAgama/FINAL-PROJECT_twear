06a6da864196d1d1b6d4ce4d6a9cdc0b69eeea65
Julia Ver <jverchyonova@gmail.com>
2020-01-14 16:23:55 +0200

    builded

---
+++ client/build/precache-manifest.ff1586941a345af4e2df427f863f3deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9481c8460f6565d933b2184ace14f420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa7445403f4ce08763f4",
+    "url": "/static/css/2.9c2eec06.chunk.css"
+  },
+  {
+    "revision": "8df4a55b88afee7b256e",
+    "url": "/static/css/main.c6c9b8ea.chunk.css"
+  },
+  {
+    "revision": "aa7445403f4ce08763f4",
+    "url": "/static/js/2.3f9a8ffd.chunk.js"
+  },
+  {
+    "revision": "8df4a55b88afee7b256e",
+    "url": "/static/js/main.2555fead.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
